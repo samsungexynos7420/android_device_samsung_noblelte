15d01c8431aec3df133f34effaa0580d6d819032
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-02-15 23:29:44 +0000

    support dynamic switching to audience path

--- device.mk
+++ device.mk
@@ -16,7 +16,8 @@
 
 # Audio
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml
+	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
+	$(LOCAL_PATH)/configs/audio/mixer_paths-audience.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0-audience.xml \
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
